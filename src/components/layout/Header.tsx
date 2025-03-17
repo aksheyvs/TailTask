26a2890428e7775b69f3de0829291674e79fd3c6
akshey <aksheyvs@gmail.com>
2025-03-17 11:45:04 +0530

    Update header styling and structure

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,8 @@
 function Header() {
     return (
-        <header>
-            <h1>TODO</h1>
-            <button>theme</button>
+        <header className="text-white flex justify-between items-center mb-[4.8rem]">
+            <h1 className="text-[3.6rem] font-[700]">TODO</h1>
+            <button className="border-none bg-transparent cursor-pointer">theme</button>
         </header>
     );
 }
